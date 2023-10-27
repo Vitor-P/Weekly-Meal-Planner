576c665fd30282ef14c4149b180fa07a1143d5cd
vitor <vitorpr000@gmail.com>
2023-10-26 22:14:06 -0300

    Connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const port = process.env.PORT || 3000;
 
