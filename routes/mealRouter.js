389e136960adf19605e20e98779d655e5bc65bde
vitor <vitorpr000@gmail.com>
2023-10-31 21:56:03 -0300

    Test and finilize crud controllers

--- routes/mealRouter.js
+++ routes/mealRouter.js
@@ -8,7 +8,7 @@ router.get("/route-test", (req, res) => {
   res.send("Server Test Successful");
 });
 
-// express.Router().route("/").get(tourController.getAllTours).post(tourController.createTour);
+// CRUD routing
 router.route("/").get(mC.getMeal).post(mC.createMeal);
 
 // router.route("/my-route").get(mC.getMeat);
