add6ea9689a6bb9eb263118e11153c58e18a458a
Vitor-P <vitorpr000@gmail.com>
2023-11-10 20:42:24 -0300

    link app to pug & start making main and style-sheet

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+const path = require("path");
 const express = require("express");
 const mealRouter = require("./routes/mealRouter");
 const ingredientRouter = require("./routes/ingredientRouter");
 
 const app = express();
 
-//1- MIDDLEWARE
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+//Global Middleware
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
 
 //Routing
+app.get("/", (req, res) => {
+  res.status(200).render("base", {
+    meal: "My Meal",
+    date: "Today",
+  });
+  // res.status(200).sendFile(path.join(__dirname, "views", "index.html"));
+});
+
 app.use("/api/v1/meals", mealRouter);
 app.use("/api/v1/ingredients", ingredientRouter);
 
